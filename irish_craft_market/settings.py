6c2eb923186775ee3c08ce66f826df0c3ff8dff2
Rebecca Stevens <rsbjshan@gmail.com>
2018-04-16 22:37:55 +0000

    Generated new django secret key and made it secret

--- irish_craft_market/settings.py
+++ irish_craft_market/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+=i!4&k-+yxt-f06=m68_0=k@v@@6+yu@hwhnft3=_q^n17@ug'
+SECRET_KEY = [os.environ.get('SECRET_KEY')]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
