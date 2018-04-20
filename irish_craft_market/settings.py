a5ae748790dd0ca1c1d60716ca8e9d0c417f2644
Rebecca Stevens <rsbjshan@gmail.com>
2018-04-20 11:27:56 +0000

    Created products.html file in products/template folder. Added products url to main urls. Created new app cart

--- irish_craft_market/settings.py
+++ irish_craft_market/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
     'accounts',
-    'products'
+    'products',
+    'cart',
     'storages',
 ]
 
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -148,6 +150,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storage.MediaStorage'
