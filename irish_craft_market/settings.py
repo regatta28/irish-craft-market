162c3bc1125b165b7b7ddcc3e022e293686c672c
Rebecca Stevens <rsbjshan@gmail.com>
2018-04-30 08:45:52 +0000

    Added new heroku app to allowed hosts

--- irish_craft_market/settings.py
+++ irish_craft_market/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = [os.environ.get('SECRET_KEY')]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'irish-craft-market.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'irish-craft-market-2.herokuapp.com']
 
 
 # Application definition
