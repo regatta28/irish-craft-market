50d090efeaf0da894a2040185feaee4ba688355a
Rebecca Stevens <rsbjshan@gmail.com>
2020-02-16 16:03:02 +0000

    Added on_delete argument to checkout and cart models.py

--- cart/models.py
+++ cart/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class CartItem(models.Model):
-    user = models.ForeignKey(User)
-    product = models.ForeignKey(Product)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
     def __str__(self):
--- checkout/models.py
+++ checkout/models.py
@@ -15,8 +15,8 @@ class Order(models.Model):
         return '{0}-{1}-{2}'.format(self.id, self.date, self.full_name)
         
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False)
-    product = models.ForeignKey(Product, null=False)
+    order = models.ForeignKey(Order, null=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(blank=False)
         
     def __str__(self):
